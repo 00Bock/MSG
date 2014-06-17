1a4a73651e94f58c2811235867ab69805f99a18f
matcode <mathias.robeck@gmail.com>
2014-06-17 21:16:45 +0200

    Karte mit Leaflet-Marker + D3-Elemente in Testordner hinzugefügt; Animation funtioniert noch nicht

---
+++ Test/D3_animation/msg_points.js
@@ -0,0 +1,21 @@
+var msg_points = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "Bezeichnun": "Windkraftanlage", "Beschreibu": "WEA Haus 1", "marker-color": "#ffec00", "marker-symbol": "star", "marker-size": "medium" }, "geometry": { "type": "Point", "coordinates": [ 13.354642592715248, 52.480709363158489 ] } },
+{ "type": "Feature", "properties": { "id": 2, "Bezeichnun": "Windkraftanlage", "Beschreibu": "WEA Haus 1", "marker-color": "#ffec00", "marker-symbol": "star"  }, "geometry": { "type": "Point", "coordinates": [ 13.355187736151281, 52.480940761989636 ] } },
+{ "type": "Feature", "properties": { "id": 3, "Bezeichnun": "Windkraftanlage", "Beschreibu": "WEA Haus 4", "marker-color": "#ffec00", "marker-symbol": "star"  }, "geometry": { "type": "Point", "coordinates": [ 13.3558540225731, 52.481477334306206 ] } },
+{ "type": "Feature", "properties": { "id": 4, "Bezeichnun": "Windmessanlage", "Beschreibu": null, "marker-color": "#ffec00", "marker-symbol": "triangle"  }, "geometry": { "type": "Point", "coordinates": [ 13.355986178557592, 52.481588001532423 ] } },
+{ "type": "Feature", "properties": { "id": 5, "Bezeichnun": "Windkraftanlage", "Beschreibu": "WEA Haus 4", "marker-color": "#ffec00", "marker-symbol": "star"  }, "geometry": { "type": "Point", "coordinates": [ 13.355892568068578, 52.481631597636024 ] } },
+{ "type": "Feature", "properties": { "id": 6, "Bezeichnun": "Windkraftanlage", "Beschreibu": "WEA Gasometer Stütze 23 (Nutzer RLI)", "marker-color": "#ffec00", "marker-symbol": "star"  }, "geometry": { "type": "Point", "coordinates": [ 13.356916776948395, 52.481252645445075 ] } },
+{ "type": "Feature", "properties": { "id": 7, "Bezeichnun": "Windmessanlage", "Beschreibu": null, "marker-color": "#ffec00", "marker-symbol": "triangle"  }, "geometry": { "type": "Point", "coordinates": [ 13.357004880938057, 52.481628244091134 ] } },
+{ "type": "Feature", "properties": { "id": 8, "Bezeichnun": "Windkraftanlage", "Beschreibu": "WEA GasometerStütze 07 (Nutzer RLI)", "marker-color": "#ffec00", "marker-symbol": "star"  }, "geometry": { "type": "Point", "coordinates": [ 13.357115010925135, 52.48167184015486 ] } },
+{ "type": "Feature", "properties": { "id": 9, "Bezeichnun": "Ladesäulen Messelbau", "Beschreibu": null, "marker-color": "#ffec00", "marker-symbol": "fuel"  }, "geometry": { "type": "Point", "coordinates": [ 13.356779623859563, 52.481797374733404 ] } },
+{ "type": "Feature", "properties": { "id": 10, "Bezeichnun": "Windkraftanlage", "Beschreibu": "WEA eflinkster Station", "marker-color": "#ffec00", "marker-symbol": "star"  }, "geometry": { "type": "Point", "coordinates": [ 13.357158553524592, 52.482247714873587 ] } },
+{ "type": "Feature", "properties": { "id": 11, "Bezeichnun": "Photovoltaikanlage", "Beschreibu": "Solaranlage InnoZ", "marker-color": "#ffec00", "marker-symbol": "square-stroked"  }, "geometry": { "type": "Point", "coordinates": [ 13.357576761208112, 52.482262233874998 ] } },
+{ "type": "Feature", "properties": { "id": 12, "Bezeichnun": "Batterie", "Beschreibu": "Batterie TU Berlin", "marker-color": "#ffec00", "marker-symbol": "circle-stroked"  }, "geometry": { "type": "Point", "coordinates": [ 13.35747111893229, 52.48243899971542 ] } },
+{ "type": "Feature", "properties": { "id": 13, "Bezeichnun": "Photovoltaik", "Beschreibu": "Solarmover", "marker-color": "#ffec00", "marker-symbol": "circle-stroked"  }, "geometry": { "type": "Point", "coordinates": [ 13.357434101623429, 52.482455631243511 ] } },
+{ "type": "Feature", "properties": { "id": 13, "Bezeichnun": "Steuerzentrale", "Beschreibu": null, "marker-color": "#ffec00", "marker-symbol": "commercial"  }, "geometry": { "type": "Point", "coordinates": [ 13.357288699302121, 52.482058471303993 ] } }
+]
+}
